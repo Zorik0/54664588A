132ee78ffc8757dfdcfb39c73796096e4adc36be
Zoriko <128587632+Zorik0@users.noreply.github.com>
2025-02-14 23:14:56 +0530

    Update TEST.lua

--- TEST.lua
+++ TEST.lua
@@ -91,19 +91,8 @@ local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/
 
 Notification:Notify(
     {Title = "SoulForge Hub", Description = "Key Link Has been Copied, Open It in browser"},
-    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "option"},
-    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
-)
-wait(1)
-Notification:Notify(
-    {Title = "SoulForge Hub", Description = "Key Link Has been Copied, Open It in browser"},
     {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "image"},
     {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84)}
-)
-wait(1)
-Notification:Notify(
-    {Title = "SoulForge Hub", Description = "Key Link Has been Copied, Open It in browser"},
-    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 5, Type = "default"}
 )
 
 end)
