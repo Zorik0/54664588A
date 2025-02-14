2eb6b1fe9cf825d08cc18a131086694dc97705dc
Zoriko <128587632+Zorik0@users.noreply.github.com>
2025-02-14 23:26:08 +0530

    Update Game_Finder.lua

--- Game_Finder.lua
+++ Game_Finder.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 10253248401 then
   print("Loading Script For Elemental Powers Tycoon")
---loadsctring here
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Zorik0/54664588A/refs/heads/main/ept.lua"))()
 else
   print("Game Not Suporrted")
 end
